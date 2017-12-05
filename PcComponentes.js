9407b3b4a383a59843a8ecb8c8d292ae7ff34c32
TheBronx <salmarina5@gmail.com>
2017-12-05 12:54:33 +0100

    pccomponentes url retriever

--- PcComponentes.js
+++ PcComponentes.js
@@ -1,11 +1,28 @@
 'use strict';
 
+var request = require('request');
 var productsRepository = require('./ProductsRepository');
 
 function retrieveProductUrl(product) {
   return new Promise((resolve, reject) => {
-    //TODO find the real URL
-    resolve('http://pccomponentes.com/' + product.id);
+    request({
+      url: 'https://eu1-search.doofinder.com/5/search?hashid=d9c752526286837ecd93ee20ff18249e&transformer=basic&rpp=1&query=' + product.pccomponentesId + '&query_counter=1&page=1',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0',
+        'Origin': 'https://www.pccomponentes.com',
+        'Referer': 'https://www.pccomponentes.com/'
+      }
+    }, function (error, response, body) {
+      if (error) reject(error);
+      else {
+        if (response && (response.statusCode === 200)) {
+          var parsed = JSON.parse(body);
+          resolve(parsed.results[0].link);
+        } else {
+          reject(new Error('PcComponentes search returned an error'));
+        }
+      }
+    });
   });
 }
 
