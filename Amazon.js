a12fbfbb22e7dcee2830869e40a001d9edad80ca
slabajos <slabajos@idealista.com>
2018-05-13 11:43:07 +0200

    parsed Amazon API response to add url and description
    updated a couple libs for security too

--- Amazon.js
+++ Amazon.js
@@ -2,14 +2,13 @@
 
 var config = require('./config.json');
 var _ = require('underscore');
+var parseString = require('xml2js').parseString;
 var productsRepository = require('./ProductsRepository');
 var OperationHelper = require('apac').OperationHelper;
 
 var opHelper = new OperationHelper(config.amazon);
 
 function eanList(products) {
-  console.dir('There are ' + products.length + ' products');
-
   return _.reduce(products, function(list, product) {
     if (!list) return '' + product.ean;
 
@@ -17,9 +16,14 @@ function eanList(products) {
   }, null);
 }
 
-function retrieveAllProductDetails(products) {
-  console.log(eanList(products));
+function findProductByEan(ean, products) {
+  for (var i=0; i<products.length; i++) {
+    if (products[i].ean == ean) return products[i];
+  }
+  return null;
+}
 
+function retrieveAllProductDetails(products) {
   return new Promise((resolve, reject) => {
     opHelper.execute('ItemLookup', {
       'SearchIndex': 'All',
@@ -29,7 +33,34 @@ function retrieveAllProductDetails(products) {
     }).then((response) => {
         //console.log("Results object: ", response.result);
         //console.log("Raw response body: ", response.responseBody);
-        resolve(products);
+        parseString(response.responseBody, function (err, result) {
+          if (err) reject(err);
+          else {
+            //console.log(JSON.stringify(result.ItemLookupResponse.Items));
+            var amazonProducts = result.ItemLookupResponse.Items[0].Item;
+            amazonProducts.forEach(amazonProduct => {
+              var ean = amazonProduct.ItemAttributes[0].EAN[0];
+              var product = findProductByEan(ean, products);
+              if (product) {
+                //console.log(amazonProduct.ItemAttributes[0].Title[0]);
+                product.amazonURL = amazonProduct.DetailPageURL[0];
+                if (amazonProduct.LargeImage) {
+                  //TODO save images
+                  //console.log(amazonProduct.LargeImage[0].URL[0]);
+                }
+                if (amazonProduct.ItemAttributes[0].ListPrice) {
+                  //TODO save price
+                  //console.log(amazonProduct.ItemAttributes[0].ListPrice[0].Amount[0]/100);
+                }
+                if (amazonProduct.ItemAttributes[0].Feature) {
+                  product.description = amazonProduct.ItemAttributes[0].Feature[0];
+                }
+              }
+            });
+
+            resolve(products);
+          }
+        });
     }).catch((err) => {
         reject(err);
     });
